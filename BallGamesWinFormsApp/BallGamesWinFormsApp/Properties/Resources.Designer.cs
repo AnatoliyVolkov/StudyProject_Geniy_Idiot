97aa1790f57f7657a2b722f2d054ceddcf2e286f
Anatoliy Volkov <Anatoliy.irl@yandex.ru>
2025-10-23 13:05:22 +0300

    Переименование файлов

--- BallGamesWinFormsApp/BallGamesWinFormsApp/Properties/Resources.Designer.cs
+++ BallGamesWinFormsApp/BallGamesWinFormsApp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BallGamesWinFormsApp.Properties {
+namespace BallButtonStopWinFormsApp.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace BallGamesWinFormsApp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BallGamesWinFormsApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BallButtonStopWinFormsApp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
